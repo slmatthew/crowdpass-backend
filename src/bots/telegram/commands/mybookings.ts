32e5d82409ce9b8284ae4d6094fe7c35c959b37b
slmatthew <36668268+slmatthew@users.noreply.github.com>
2025-05-04 03:32:36 +0300

    refactor: update mybookingsCommand to use SharedContext for type safety

--- src/bots/telegram/commands/mybookings.ts
+++ src/bots/telegram/commands/mybookings.ts
@@ -1,7 +1,8 @@
-import { CommandContext, Context } from "grammy";
+import { CommandContext } from "grammy";
 import { sendBookingsPage } from "../utils/paginator";
+import { SharedContext } from "@/types/grammy/SessionData";
 
-export const mybookingsCommand = async (ctx: CommandContext<Context>) => {
+export const mybookingsCommand = async (ctx: CommandContext<SharedContext>) => {
   const userId = ctx.from?.id.toString();
   if (!userId) return;
   await sendBookingsPage(ctx, userId, 1);
