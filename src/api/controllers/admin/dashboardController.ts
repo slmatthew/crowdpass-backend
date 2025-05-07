cb3e13c56a8ff3d4f7b6ad7662c4cea9d5eec2c7
slmatthew <36668268+slmatthew@users.noreply.github.com>
2025-05-07 18:18:39 +0300

    refactor: remove unused methods and clean up dashboard controller

--- src/api/controllers/admin/dashboardController.ts
+++ src/api/controllers/admin/dashboardController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
-import { prisma } from "../../../db/prisma";
-import { UserService } from "../../../services/userService";
-import { AdminDashboardService } from "../../../services/adminDashboardService";
-import { OrganizerService } from "../../../services/organizerService";
+import { UserService } from "@/services/userService";
+import { AdminDashboardService } from "@/services/adminDashboardService";
 
 export const dashboardController = {
   async getSummary(req: Request, res: Response) {
@@ -30,38 +28,5 @@ export const dashboardController = {
     }
 
     res.json(user);
-  },
-
-  /**
-   * @TODO перекинуть это все в отдельные контроллеры
-   * @TODO убрать вызовы напрямую к БД
-   */
-  async getOrganizers(req: Request, res: Response) {
-    if(req.user?.role === 'MANAGER') {
-      const user = await UserService.findUserById(req.user.id);
-  
-      const organizerId = user?.admin?.organizerId;
-      if(organizerId) {
-        const organizers = await OrganizerService.getAdminAvailableOrganizers(organizerId);
-        return res.json(organizers);
-      }
-    }
-  
-    const organizers = await OrganizerService.getAdminAvailableOrganizers();
-    res.json(organizers);
-  },
-  async getCategories(req: Request, res: Response) {
-    const categories = await prisma.category.findMany();
-    res.json(categories);
-  },
-  async getSubcategories(req: Request, res: Response) {
-    const categoryId = req.params.categoryId
-      ? parseInt(req.params.categoryId as string)
-      : undefined;
-
-    const where = categoryId ? { categoryId } : undefined;
-
-    const subcategories = await prisma.subcategory.findMany({ where });
-    res.json(subcategories);
   },
 };
