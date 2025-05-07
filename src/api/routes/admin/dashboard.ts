d43575979e4e497ad3759f23ab04fb6b66a1605e
slmatthew <36668268+slmatthew@users.noreply.github.com>
2025-05-07 18:19:10 +0300

    refactor: remove metadata routes from dashboard

--- src/api/routes/admin/dashboard.ts
+++ src/api/routes/admin/dashboard.ts
@@ -6,16 +6,4 @@ const router = Router();
 router.get('/summary', dashboardController.getSummary);
 router.get('/me', dashboardController.getMe);
 
-/**
- * @TODO вынести это все в отдельные специализированные роуты
- * 
- * пока что так, но в будущем, когда начнется реализация управления категориями,
- * организаторами и т.д. - нужно будет сделать отдельные роуты,
- * а фронт должен будет получать данные из этих роутов:
- * /admin/dashboard/metadata/organizers -> /admin/organizers
- */
-router.get('/metadata/organizers', dashboardController.getOrganizers);
-router.get('/metadata/categories', dashboardController.getCategories);
-router.get('/metadata/categories/:categoryId/subcategories', dashboardController.getSubcategories);
-
 export default router;
