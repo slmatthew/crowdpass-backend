9e44bc6731f9cfcc60627d1c23caa2c53c7c900a
slmatthew <36668268+slmatthew@users.noreply.github.com>
2025-05-20 23:16:01 +0300

    fix: correct error message in updateOrganizer function

--- src/api/controllers/admin/organizersController.ts
+++ src/api/controllers/admin/organizersController.ts
@@ -61,7 +61,7 @@ export async function updateOrganizer(req: Request, res: Response) {
     const updated = await OrganizerService.update(id, { name, description, contacts });
     res.status(200).json(updated);
   } catch(err) {
-    res.status(500).json({ message: 'Не удалось создать организатора' });
+    res.status(500).json({ message: 'Не удалось обновить организатора' });
   }
 }
 
